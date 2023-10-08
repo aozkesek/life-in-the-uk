249a1b32160446b0afcf2baefe4ff8b87e9cac1f
Ahmet OZKESEK <aozkesek@gmail.com>
2023-10-08 18:16:28 +0100

    add continue command line argument

--- lifeintheuk.py
+++ lifeintheuk.py
@@ -6,13 +6,16 @@ Created on Sat Aug 26 09:49:05 2023
 @author: ahmetozkesek
 """
 
-def __fill_qa_from(fname: str)-> tuple:
+__fname = "life-in-the-uk.qa.txt"
+__cname = "life-in-the-uk.qa.cont"
+
+def __fill_qa_from(cont: bool = False)-> tuple:
 	import random
 	import time
 
 	# qas is a dictionary where key=question, value=answers
 	qas = {}
-	with open(fname, encoding="unicode-escape") as lituk:
+	with open(__fname) as lituk:
 		qa = ""
 		
 		for ln in lituk.readlines():
@@ -28,23 +31,39 @@ def __fill_qa_from(fname: str)-> tuple:
 				if len(qa) > 0:
 					assert len(qas[qa]) in [2,4], qa
 				
-				qa = ln[:-1]
+				#qa = ln[:-1]
+				qa = ln
 				# empties, then refills the duplicates
 				qas[qa] = [] 
 	
-	# shuffle the questions
-	random.seed(time.clock_gettime_ns(0))
-	# FIX: population must be a sequence not a dict!!!
-	qi = random.sample(list(qas), len(qas)) 
-
+	qi = None
+	if cont:
+		try:
+			# fill qi from the file where they left the previous exam
+			with open(__cname,"r") as lituk:
+				qi = lituk.readlines()
+		except FileNotFoundError:
+			# skip if the file does not exist, qi will be checked later
+			pass
+		
+	# check qi in case of something went wrong while we try to load it above
+	if not cont or qi is None:
+		# shuffle the questions
+		random.seed(time.clock_gettime_ns(0))
+		qi = random.sample(list(qas), len(qas)) 
+		qi.append("\n[0]")
+		# write qi into a file, so they can continue next time where they left
+		with open(__cname,"w") as lituk:
+			lituk.writelines(qi)
+			
 	return (qas, qi)		
 
 def __lituk_print(qas: dict, qi: list):
-	fname = "lituk-tests"
 	
 	i = 0
 	que = {}
 	ans = {}
+	fname = "lituk-exams"
 	
 	# fill each test with a set of 24 questions
 	for q in qi:
@@ -107,20 +126,44 @@ def __txt2pdf(fname):
 	# txt and ps are not needed anymore, remove them
 	subprocess.run(["rm", fname+".txt", fname+".ps"])
 
-def __lituk_test(qas: dict, qi: list, ndx: int = 0):
+def __lituk_test(qas: dict, qi: list, prv: int = 0):
+	curr = prv
+	for i in range(prv, len(qas) // 24):
+		try:
+			__lituk_exam(qas, qi, i)
+			curr = i + 1
+		except KeyboardInterrupt:
+			break
+		
+		print("")
+		
+		try:
+			c = input("press C+Enter for continue, or any key to end ->")
+			if c != "c" and c != "C":
+				break
+		except KeyboardInterrupt:
+			break
+	
+	qi[len(qi) - 1] = f"\n[{curr}]"
+	with open(__cname,"w") as lituk:
+		lituk.writelines(qi)
+
+def __lituk_exam(qas: dict, qi: list, ndx: int = 0):
 	import os
 	
 	# ai holds answers (True/False, Question)
 	ai = []
 	# ask only a set of 24 questions
 	p = ndx * 24
-
+	n = 1
+	
 	for q in qi[p : p+24]:
 		os.system("clear")
 
-		print(f"Life in the UK Test [{p+1} / {len(qi) // 24}]:\n")
+		print(f"Life in the UK Test [{(p+1)//24} / {len(qi) // 24}]:\n")
 		print(f"The tests and {len(qi)} questions are shuffle everytime.\n")
-		print(f"Q->{q}")
+		print(f"Q-{p%24+n} -> {q}")
+		n = n + 1
 		
 		i = 0
 		for j in qas[q]:
@@ -157,10 +200,11 @@ def __lituk_test(qas: dict, qi: list, ndx: int = 0):
 			for j in qas[i[1]]:
 				print("\t", j)
 	# yo have to get right at least 75% correct to PASS the exam.
+	print("you have done the exam, the result is;")
 	if t / 24 >= 0.75:
-		print(t, "C,", 24-t, "W > PASSED.")
+		print(t, "Correct,", 24-t, "Wrong > PASSED.")
 	else:
-		print(t, "C,", 24-t, "W > FAILED")
+		print(t, "Correct,", 24-t, "Wrong > FAILED")
 	
 def __lituk_distance(qas):
 	# pick one of the basic algo 
@@ -178,31 +222,33 @@ def __lituk_distance(qas):
 			# a score of 1 is 100% same, the bigger result the different
 			if res < 15:
 				print("\t", res, t)
-	
 
 if __name__ == "__main__":
 	import sys
+
+	has_arg = len(sys.argv) > 1
+	is_cont = has_arg and sys.argv[1] == "cont"
+	is_dump = has_arg and sys.argv[1] == "dump"
+	is_dist = has_arg and sys.argv[1] == "dist"
+	is_prnt = has_arg and sys.argv[1] == "print"
 	
-	qas, qi = __fill_qa_from("life-in-the-uk.qa.txt")
+	qas, qi = __fill_qa_from(is_cont)
+	prv = int(qi[len(qi)-1].replace("\n","").replace("[","").replace("]",""))
+	qi = qi[:-1]
 	
-	if len(sys.argv) > 1:
-		if sys.argv[1] == "print":
+	if has_arg:
+		if is_prnt:
 			__lituk_print(qas, qi)
-		elif sys.argv[1] == "dump":
+		elif is_dump:
 			for q in sorted(list(qas)):
 				print(q)
-		elif sys.argv[1] == "dist":
+		elif is_dist:
 			__lituk_distance(qas)
+		elif is_cont:
+			__lituk_test(qas, qi, prv)
 		else:
 			print("usage:\n\n\tpython lifeintheuk.py [print|dump|dist]")
 		sys.exit(0)
 			
-	for i in range(len(qas) // 24):
-		__lituk_test(qas, qi, i)
-		
-		print("")
-		
-		c = input("press C+Enter for continue, or any key to end ->")
-		if c != "c" and c != "C":
-			break
-
+	__lituk_test(qas, qi, prv)
+	
